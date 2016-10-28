6d0aa4e4941abef595a674aa21db2ca4c0f21ad7
Rawin.ngamloet <Rawin.n@augmentis.biz>
2016-10-28 18:22:11 +0700

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+## For Devstack beginner
+### Installation
+* Download and install [Vagrant](https://www.vagrantup.com)
+* Run start vagrant
+			vagrant up
+* SSH to the vagrant box
+			vagrant ssh
+* Initialize Devstack
+			cd devstack; ./stack.sh
---
+++ install-script.sh
@@ -0,0 +1,23 @@
+DEVSTACK_DIR=devstack
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y git
+
+# to devstack
+if [ ! -d "$DEVSTACK_DIR" ]; then
+  git clone https://git.openstack.org/openstack-dev/devstack $DEVSTACK_DIR
+fi
+
+# owner
+if [ -d "$DEVSTACK_DIR" ]; then
+  chown -R ubuntu:ubuntu $DEVSTACK_DIR
+fi
+
+cd $DEVSTACK_DIR
+
+if [[ "$PWD" =~ $DEVSTACK_DIR ]]; then
+  echo '[[local|localrc]]\nADMIN_PASSWORD=admin\nDATABASE_PASSWORD=$ADMIN_PASSWORD\nRABBIT_PASSWORD=$ADMIN_PASSWORD\nSERVICE_PASSWORD=$ADMIN_PASSWORD' > local.conf
+  chown ubuntu:ubuntu local.conf
+  echo 'created: local.conf'
+fi
